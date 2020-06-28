58f1df99138e3949c0c1bd186cbc9455650cd26e
sa-abhi <sm.sultanul.azam@gmail.com>
2020-06-29 01:02:35 +0600

    first commit

---
+++ DataAccess.EFCore/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DataAccess.EFCore
+{
+   public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a simple ASP.NetCore Project following Repository Pattern
+## Developed by S M Sultanul Azam
